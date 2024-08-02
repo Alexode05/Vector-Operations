39f7cd4ad718043ce78b3bf2dca690baa4663c5b
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-08-03 01:11:43 +0200

    start the project

---
+++ vectorOperations.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+    string dimension;
+    int opertation;
+
+
+    cout << "########## Operations on vertors ##########" << '\n';
+    cout << "What's dimension are the vectors ? (2D/3D):" << '\n';
+    do{
+        cin >> dimension;
+    }while (dimension != "2D" && dimension != "3D");
+    cout << "What operation do you want to perform ?" << '\n';
+    cout << "1. Norm" << '\n' << "2. Saclar product" << '\n' << "3. Vectorial product (only for 3D) " << '\n';
+    do{
+        cin >> opertation;
+    }while(opertation !=1 && opertation !=2 && opertation !=3 || (dimension == "2D" && opertation == 3));
+
+    return 0;
+}
+
