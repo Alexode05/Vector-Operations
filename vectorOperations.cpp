2d8bd52dba836bbb115e0cde7fe591e00366fab6
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-08-03 16:02:50 +0200

    new branch

--- vectorOperations.cpp
+++ vectorOperations.cpp
@@ -16,7 +16,7 @@ int main() {
     string dimension;
     int dim;;
     int operation;
-    vector<double> v1, v2;
+    vector<double> v1, v2, v3;
 
     cout << "########## Operations on vertors ##########" << '\n';
     while(true){
@@ -25,7 +25,12 @@ int main() {
             cin >> dimension;
         }while ((dimension != "2D" && dimension != "3D") && (dimension != "2d" && dimension != "3d"));
         cout << "What operation do you want to perform ?" << '\n';
-        cout << "1. Norm" << '\n' << "2. Saclar product" << '\n' << "3. Vectorial product (only in 3D) " << '\n';
+        cout << "1. Norm" << '\n' 
+            << "2. Saclar product" << '\n' 
+            << "3. Vectorial product (only in 3D) " << '\n'
+            << "4. Angle between two vectors" << '\n'
+            << "5. Area of the parallelogram built on 2 vectors" << '\n'
+            << "6. Volume of the parallelepiped built on 3 vectors" << '\n';
         do{
             cin >> operation;
         }while((operation !=1 && operation !=2 && operation !=3) || (dimension == "2D" && operation == 3) || (dimension == "2d" && operation == 3));
@@ -46,6 +51,7 @@ int main() {
             enterValues(v2, dim);
             cout << "The scalar product is: " <<  dotProduct(v1, v2, dim) <<  '\n';
             break;
+
         case 3:
             cout << "Enter the values of the  1st vector: v1 =" << '\n';
             enterValues(v1, dim);
@@ -57,6 +63,10 @@ int main() {
                 cout << "|" <<  value << "|" << '\n';
             }
             break;
+
+        case 4:
+        case 5:
+        case 6:    
         default:
             break;
         }
