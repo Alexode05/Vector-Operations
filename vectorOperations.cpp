637ff5adad4a8ca80a47adef4b7e057473a68252
Alexode05 <alexandre.rossetti2005@gmail.com>
2024-08-03 14:43:00 +0200

    add the cross product function

--- vectorOperations.cpp
+++ vectorOperations.cpp
@@ -9,6 +9,8 @@ using namespace std;
 int dimensionOfVector(string dimension);
 void enterValues(vector<double>& v, int dim);
 double norm(vector<double>& v);
+double dotProduct(vector<double>& v1, vector<double>& v2, int dim);
+vector<double> crossProduct(vector<double>& v1, vector<double>& v2);
 
 int main() {
     string dimension;
@@ -36,15 +38,27 @@ int main() {
         cout << "The norm of the vector is: " << norm(v1) << '\n';
         break;
     
+    case 2:
+        cout << "Enter the values of the  1st vector: v1 =" << '\n';
+        enterValues(v1, dim);
+        cout << "Enter the values of the  2nd vector: v2 =" << '\n';
+        enterValues(v2, dim);
+        cout << "The scalar product is: " <<  dotProduct(v1, v2, dim) <<  '\n';
+        break;
+    case 3:
+        cout << "Enter the values of the  1st vector: v1 =" << '\n';
+        enterValues(v1, dim);
+        cout << "Enter the values of the  2nd vector: v2 =" << '\n';
+        enterValues(v2, dim);
+        
+        cout << "The vectorial product is: " << '\n';
+        for(double value : crossProduct(v1, v2)){
+            cout << "|" <<  value << "|" << '\n';
+        }
+        break;
     default:
         break;
     }
-    
-    
-    /* for(int i=0; i<dim; i++){
-        cout << v1[i] << " ";
-    } */
-
     return 0;
 }
 
@@ -68,3 +82,18 @@ double norm(vector<double>& v){
     return sqrt(sum);
 }
 
+ double dotProduct(vector<double>& v1, vector<double>& v2, int dim){
+    double sum = 0.0;
+    for(int i=0; i<dim; i++){
+        sum += (v1[i] * v2[i]);
+    }
+    return sum;
+}
+
+vector<double> crossProduct(vector<double>& a, vector<double>& b){
+    vector<double> result;
+    result.push_back(a[1]*b[2] - a[2]*b[1]);
+    result.push_back(a[2]*b[0] - a[0]*b[2]);
+    result.push_back(a[0]*b[1] - a[1]*b[0]);
+    return result;
+}
